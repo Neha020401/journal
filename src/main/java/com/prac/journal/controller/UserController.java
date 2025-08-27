485c1a9c87ac7e84d4633148f0d6f29ee4f201ef
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2025-08-28 02:17:12 +0530

    User password encryption

--- src/main/java/com/prac/journal/controller/UserController.java
+++ src/main/java/com/prac/journal/controller/UserController.java
@@ -32,7 +32,8 @@ public class UserController {
     @PostMapping
     public ResponseEntity<?> createEntry(@RequestBody User user){
         try {
-            userService.saveEntry(user);
+//            userService.saveEntry(user);
+            userService.saveNewUser(user);
             return new ResponseEntity<>(user, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>("User with this username already exists!", HttpStatus.BAD_REQUEST);
