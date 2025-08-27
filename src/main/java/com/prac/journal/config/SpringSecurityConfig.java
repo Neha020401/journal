9fe8901a33db183399aa23c480f92524fa2c8f39
Neha Yadav <143873299+Neha020401@users.noreply.github.com>
2025-08-27 18:40:55 +0530

    UserDetail imp

--- src/main/java/com/prac/journal/config/SpringSecurityConfig.java
+++ src/main/java/com/prac/journal/config/SpringSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.prac.journal.config;
 
+import com.prac.journal.service.UserServiceDetailImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -10,15 +15,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    @Autowired
+    private UserServiceDetailImp userServiceDetailImp ;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/journal/**").authenticated()
                         .anyRequest().permitAll()
-                )
-                .httpBasic();
+                ).httpBasic();
 
         return http.build();
+    }
+
+    public void  configure(AuthenticationManagerBuilder auth )throws  Exception {
+        auth.userDetailsService(userServiceDetailImp).passwordEncoder(passwordEncoder())  ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
 }
